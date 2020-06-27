c266b8cea058a94a80b2402a5cfc48c11b0b8c48
Nick Ang <nickangtc@gmail.com>
2020-06-27 12:50:17 +0200

    Add basic dependencies and express structure

---
+++ routes/routes-index.js
@@ -0,0 +1,10 @@
+const express = require('express')
+
+const router = express.Router()
+
+/* GET home page. */
+router.get('/', function root(req, res) {
+    res.render('index.html')
+})
+
+module.exports = router
