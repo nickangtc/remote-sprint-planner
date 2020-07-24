9780069d3cbb6568babbb8e73a0bf87ac2da62b3
Nick Ang <nickangtc@gmail.com>
2020-07-24 16:42:21 +0200

    Minor UI improvements

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -1,9 +1,11 @@
 window.addEventListener('DOMContentLoaded', () => {
     const startBtn = document.querySelector('#start')
+    const startHelpText = document.querySelector('#start-help-text')
     const sprintNameForm = document.querySelector('#sprint-name-form')
 
     function start() {
         this.classList.add('d-none')
+        startHelpText.classList.add('d-none')
         sprintNameForm.classList.remove('d-none')
     }
 
